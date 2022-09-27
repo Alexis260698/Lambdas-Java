4cf18eead60b1c4f9bac5acdf982cfbd2b37df2d
Alexis Hernandez <ahernann@emeal.nttdata.com>
2022-09-27 11:05:19 -0500

    FIRST COMMIT

---
+++ src/LambdaTest.java
@@ -0,0 +1,18 @@
+
+public class LambdaTest {
+    public static void main(String[] args) {
+        FunctionTest ft= () -> System.out.println("HOLA MUNDO");
+
+        LambdaTest objeto = new LambdaTest();
+
+        objeto.miMetodo(ft);
+        
+    }
+
+    public void miMetodo(FunctionTest parametro){
+
+        parametro.saludar();
+
+    }
+}
+
